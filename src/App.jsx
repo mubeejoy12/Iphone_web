09be240834e1931701dc1dd7df45bdd9d9fbea8a
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-11-12 15:23:09 +0100

    implemmented new componnent on app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-
-import './App.css'
+import Navbar from "./component/Navbar";
+import Hero from "./component/Hero";
+import HighLights from "./component/Highlights";
 
 function App() {
-
-
   return (
-    <>
-     
-    </>
-  )
+    <main className="bg-black">
+      <Navbar /> <Hero /> <HighLights />
+    </main>
+  );
 }
 
-export default App
+export default App;
