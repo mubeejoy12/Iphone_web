1961683c534d5f744eec9d3fdfdab382274b4b07
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-11-16 11:44:45 +0100

    core import and animation

---
+++ src/component/Model.jsx
@@ -0,0 +1,11 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap";
+import ModelView from "./ModelView";
+import { useEffect, useRef, useState } from "react";
+import { yellowImg } from "../utils";
+
+import * as THREE from 'three';
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
+import { models, sizes } from "../constant";
+import { animateWithGsapTimeline } from "../utils/animatioons";
