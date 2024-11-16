9a9b6a88656f4c32290f7b4a9636767c25a46869
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-11-16 11:37:59 +0100

    gsap animation setUp

--- src/component/VideoCarousel.jsx
+++ src/component/VideoCarousel.jsx
@@ -24,3 +24,18 @@ const VideoCarousel = () => {
 
   const [loadedData, setLoadedData] = useState([]);
   const { isEnd, isLastVideo, startPlay, videoId, isPlaying } = video;
+
+  useGSAP(() => {
+    // slider animation to move the video out of the screen and bring the next video in
+    gsap.to("#slider", {
+      transform: `translateX(${-100 * videoId}%)`,
+      duration: 2,
+      ease: "power2.inOut", // show visualizer https://gsap.com/docs/v3/Eases
+    });
+
+    // video animation to play the video when it is in the view
+    gsap.to("#video", {
+      scrollTrigger: {
+        trigger: "#video",
+        toggleActions: "restart none none none",
+      },
