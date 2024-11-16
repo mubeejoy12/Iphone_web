c1242c69b59a1969432580a592dab7064b9944ce
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-11-16 11:58:07 +0100

    Add responsive Hero section with adaptive video and GSAP animations

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { heroVideo, smallHeroVideo } from "../utils";
+
+const Hero = () => {
   const [videosrc, setVideosrc] = useState(
     window.innerWidth < 760 ? smallHeroVideo : heroVideo
   );
@@ -19,6 +21,7 @@ import { heroVideo, smallHeroVideo } from "../utils";
     return () => {
       window.removeEventListener("resize", handleVideosrc);
     };
+  }, []);
   useGSAP(() => {
     gsap.to("#hero", {
       opacity: 1,
@@ -30,3 +33,36 @@ import { heroVideo, smallHeroVideo } from "../utils";
       delay: 2,
     });
   }, []);
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">
+          iphone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12 ">
+          <video
+            autoPlay
+            muted
+            playsInline={true}
+            key={videosrc}
+            className="pointer-events-none"
+          >
+            <source src={videosrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
