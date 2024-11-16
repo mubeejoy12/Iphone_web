0cbf65635e63f8a7ee0b5c1a101046ffea8a3691
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-11-16 11:47:40 +0100

    effects and animation

--- src/component/Model.jsx
+++ src/component/Model.jsx
@@ -16,3 +16,24 @@ const Model = () => {
     color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
     img: yellowImg,
   })
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if(size === 'large') {
+      animateWithGsapTimeline(tl, small, smallRotation, '#view1', '#view2', {
+        transform: 'translateX(-100%)',
+        duration: 2
+      })
+    }
+
+    if(size ==='small') {
+      animateWithGsapTimeline(tl, large, largeRotation, '#view2', '#view1', {
+        transform: 'translateX(0)',
+        duration: 2
+      })
+    }
+  }, [size])
+
+  useGSAP(() => {
+    gsap.to('#heading', { y: 0, opacity: 1 })
+  }, []);
