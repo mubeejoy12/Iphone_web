f3fc6ebfa9756633a6dea81906a504c6f1e8d0d2
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-11-16 11:57:14 +0100

    responsive  video handling

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,3 +2,20 @@ import React, { useEffect, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { heroVideo, smallHeroVideo } from "../utils";
+  const [videosrc, setVideosrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideosrc = () => {
+    if (window.innerWidth < 760) {
+      setVideosrc(smallHeroVideo);
+    } else {
+      setVideosrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideosrc);
+    return () => {
+      window.removeEventListener("resize", handleVideosrc);
+    };
