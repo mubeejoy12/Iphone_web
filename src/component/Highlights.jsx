08ef9f0b253e4e6e85d520d37a3ebac8cb2ff9ec
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-11-16 11:55:22 +0100

    gsap animation setup

--- src/component/Highlights.jsx
+++ src/component/Highlights.jsx
@@ -1,3 +1,15 @@
+  useGSAP(() => {
+    gsap.to("#title", {
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".link", {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.25,
+    });
+  }, []);
     <section
       id="highlight"
       className="w-full overflow-hidden h-full common-padding bg-zinc"
