12b6328129d617502b2e118dbcc927220d20eea0
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-11-16 11:45:08 +0100

    component state and ref

--- src/component/Model.jsx
+++ src/component/Model.jsx
@@ -9,3 +9,10 @@ import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constant";
 import { animateWithGsapTimeline } from "../utils/animatioons";
+const Model = () => {
+  const [size, setSize] = useState('small');
+  const [model, setModel] = useState({
+    title: 'iPhone 15 Pro in Natural Titanium',
+    color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+    img: yellowImg,
+  })
