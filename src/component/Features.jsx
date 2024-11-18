06ed17b4f2a66888dda20f80338e4f19223299ca
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-11-18 08:46:29 +0100

    core imports

---
+++ src/component/Features.jsx
@@ -0,0 +1,5 @@
+import { useGSAP } from "@gsap/react";
+import React, { useRef } from "react";
+import gsap from "gsap";
+import { explore1Img, explore2Img, exploreVideo } from "../utils";
+import { animateWithGsap } from "../utils/animatioons";
