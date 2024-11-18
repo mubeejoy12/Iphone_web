ca262280773bb491a0cbe28ad059075102f7045f
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-11-18 08:36:50 +0100

    essential imports

--- src/component/HowItsWorks.jsx
+++ src/component/HowItsWorks.jsx
@@ -1,3 +1,9 @@
+
+import React, { useRef } from "react";
+import { chipImg, frameImg, frameVideo } from "../utils";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { animateWithGsap } from "../utils/animatioons";
   const videoRef = useRef();
 
   useGSAP(() => {
