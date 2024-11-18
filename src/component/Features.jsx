f4519e2c2cfe0985baac9842fe153707797e3829
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-11-18 08:47:28 +0100

    GSAP Animations Configuration

--- src/component/Features.jsx
+++ src/component/Features.jsx
@@ -4,3 +4,28 @@ import gsap from "gsap";
 import { explore1Img, explore2Img, exploreVideo } from "../utils";
 import { animateWithGsap } from "../utils/animatioons";
   const videoRef = useRef();
+  useGSAP(() => {
+    gsap.to("#exploreVideo", {
+      scrollTrigger: {
+        trigger: "#exploreVideo",
+        toggleActions: "play pause reverse restart",
+        start: "-10% bottom",
+      },
+      onComplete: () => {
+        videoRef.current.play();
+      },
+    });
+
+    animateWithGsap("#features_title", { y: 0, opacity: 1 });
+    animateWithGsap(
+      ".g_grow",
+      { scale: 1, opacity: 1, ease: "power1" },
+      { scrub: 5.5 }
+    );
+    animateWithGsap(".g_text", {
+      y: 0,
+      opacity: 1,
+      ease: "power2.inOut",
+      duration: 1,
+    });
+  }, []);
