96c2c4c1a67ef2646de89f7aa0fca49a86edd010
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-11-18 08:50:52 +0100

    added some new components

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,21 @@ import Navbar from "./component/Navbar";
 import Hero from "./component/Hero";
 import HighLights from "./component/Highlights";
 import Model from "./component/Model";
+// import * as Sentry from "@sentry/react";
+import Features from "./component/Features";
+import HowItsWorks from "./component/HowItsWorks";
+import Footer from "./component/Footer";
 
 function App() {
   return (
     <main className="bg-black">
-      <Navbar /> <Hero /> <HighLights /> <Model />
+      <Navbar />
+      <Hero />
+      <HighLights />
+      <Model />
+      <Features />
+      <HowItsWorks />
+      <Footer />
     </main>
   );
 }
